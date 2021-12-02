c2e9774286cc1ab47d362b74b7a6debf6f7c5c57
Emmanuel Alejandro Zamudio Esparza <alexesparzalex@gmail.com>
2021-12-01 20:52:44 -0600

    Actualización 01 - 12 : 08:50pm
    Avances en:
    
    Interfaz General de Administrador
    Inicio de Sesión
    Tabla de Artesanos
    Tabla de Productos

---
+++ tai-admin/admin_home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Panel de Control: Inicio</title>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <?php
+            include 'admin_menu_style.php';
+            ?>
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['admin_nombre']; ?></span>
+                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Mi Perfil
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Salir
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Holasdsdf</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; TAI COMMERCE: Administrador <i>2021</i></span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+</body>
+
+</html>
---
+++ tai-admin/sql_query_php/admin_login.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    require 'connect_bd.php';
+    
+
+    $admin_email = $_POST['admin_email'];
+    $admin_pass = $_POST['admin_pass'];
+
+    $login = mysqli_query($conexion, "SELECT admin_nombre FROM tai_admin WHERE admin_email = '$admin_email' AND admin_pass = '$admin_pass'");
+    $num_login = mysqli_num_rows($login);
+    if ($num_login == 0) {
+        echo'<script type="text/javascript">alert("¡USUARIO O CONTRASEÑA INCORRECTA!");window.location.href="../index.php";</script>';
+    } else {
+        $row_usuario = mysqli_fetch_assoc($login);
+        $_SESSION['admin_nombre'] = $row_usuario['admin_nombre'];
+        header('location:../admin_home.php');
+    }
